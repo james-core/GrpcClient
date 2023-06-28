a22006601899a2f4db6bf5943f08193538fdc445
zhengpeng <zhengpeng@7net.cc>
2023-06-28 16:23:24 +0800

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcService;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("https://localhost:7279");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "我是客户端" });
+Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
+Console.ReadKey();
